57a9db1bc71be3a8585c33d60b18b490fc4bf500
Ebenézer Dorneles <ebenezerdorneles@gmail.com>
2021-12-08 15:55:45 -0300

    Mensagens para o usuário - backend

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -5,28 +5,31 @@ angular
 
     $scope.filtro = "";
 
-    var Contato = $resource('/contatos/:id');
+    $scope.mensagem = { texto: "" };
 
-    function buscaContatos(){
+    var Contato = $resource("/contatos/:id");
+
+    function buscaContatos() {
       Contato.query(
-        function(contatos){
+        function (contatos) {
           $scope.contatos = contatos;
         },
-        function(erro){
-          console.log(`Não foi possivel obter a lista de contatos!`);
+        function (erro) {
           console.log(erro);
+          $scope.mensagem = {
+            texto: `Não foi possivel obter a lista de contatos!`,
+          };
         }
       );
     }
     buscaContatos();
 
-    $scope.remove = function(contato){
-      Contato.delete({id: contato._id}, 
-        buscaContatos,
-        function(erro){
-          console.log('não foi possivel remover o contato');
-          console.log(erro);
-        } 
-        );
-    }
+    $scope.remove = function (contato) {
+      Contato.delete({ id: contato._id }, buscaContatos, function (erro) {
+        console.log(erro);
+        $scope.mensagem = {
+          texto: "não foi possivel remover o contato",
+        };
+      });
+    };
   });
