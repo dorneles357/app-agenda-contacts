d0cd9d5bd32ceab162a7a626b74cd2dee4a3e54e
Ebenézer Dorneles <ebenezerdorneles@gmail.com>
2021-12-29 18:46:38 -0300

    conexão com o banco de dados

---
+++ config/database.js
@@ -0,0 +1,26 @@
+
+const mongoose = require('mongoose');
+
+module.exports = (uri) =>{
+	mongoose.connect(uri);
+
+	mongoose.connection.on('connected', ()=>{
+		console.log(`Connected!`);
+	});
+	mongoose.connection.on('disconnected', ()=>{
+		console.log(`Disconnected!`);
+	});
+	mongoose.connection.on('error', (erro)=>{
+		console.log(`Erro:${erro}`);
+	});
+
+	//close connection
+	process.on('SIGINT', ()=>{
+		mongoose.connection.close(()=>{
+			console.log(`Disconnected application ...`);
+
+			process.exit(0);
+		});
+	});
+}
+
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const app = require('./config/express')();
+require('./config/database.js')('mongodb://127.0.0.1/server');
+
 
 http.createServer(app).listen(app.get('port'), () => {
 	console.log(`Express server escutando na porta ${app.get('port')}`);
