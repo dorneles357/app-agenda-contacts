ae0b0d8e539e7036be8d07a25e6c97fd7518e7b9
Ebenézer Dorneles <ebenezerdorneles@gmail.com>
2021-12-30 14:23:50 -0300

    add: modelo para armazenamento no banco de dados

---
+++ app/models/contatos.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+module.exports = ()=>{
+	const schema = mongoose.Schema({
+		nome:{type:String, required: true},
+		email:{type: String, required: true, index: {unique: true}}
+	});
+	return mongoose.model('Contato', schema);
+}
