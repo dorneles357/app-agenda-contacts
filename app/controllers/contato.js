6a111e69a4eaa61aa5b0728a78838598cf1ffa50
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-02-16 17:13:46 -0300

    ✅ atualização

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -24,11 +24,13 @@ module.exports = (app) =>{
 				},
 				(erro) =>{
 					console.log(erro);
+					res.status(404).json(erro);
 				}
 			)
 	};
 	controller.removeContato = (req, res)=>{
 		const _id = req.params.id;
+
 		Contato.remove({"_id": _id}).exec()
 			.then(
 				()=>{
@@ -38,34 +40,34 @@ module.exports = (app) =>{
 					return console.log(erro);
 				}
 			);
-	};
-	controller.salvaContato = (req, res) =>{
-		const _id = req.body._id;
-		if(_id){
-			Contato.findByIdAndUpdate(_id, req.body).exec()
-				.then(
-					(contato) => {
-						res.json(contato);
-					},
-					(erro) => {
-						console.log(erro);
-						res.status(500).json(erro);
-					}
-				);
-			
-		}else{
-			Contato.create(req.body)
-				.then(
-					(contato) =>{
-						res.status(201).json(contato);
-					},
-					(erro)=>{
-						console.log(erro);
-						res.status(500).json(erro);
-					}
-				);
+		};
+		controller.salvaContato = (req, res) =>{
+			const _id = req.body._id;
+			if(_id){
+				Contato.findByIdAndUpdate(_id, req.body).exec()
+					.then(
+						(contato) => {
+							res.json(contato);
+						},
+						(erro) => {
+							console.log(erro);
+							res.status(500).json(erro);
+						}
+					);
+				
+			}else{
+				Contato.create(req.body)
+					.then(
+						(contato) =>{
+							res.status(201).json(contato);
+						},
+						(erro)=>{
+							console.log(erro);
+							res.status(500).json(erro);
+						}
+					);
+			}
 		}
-	}
 
 	return controller;
 };
